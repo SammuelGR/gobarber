e85a1c22dc7a14f5c607ab88493c0dc88c46bcac
Sammuel Reis <sammugr@hotmail.com>
2020-02-05 21:22:57 -0300

    user migration

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-	const user = await User.create({
-		name: 'Sammuel Reis',
-		email: 'sammuel@rocketseat.com.br',
-		password_hash: '1231231231',
-	});
-
-	return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
