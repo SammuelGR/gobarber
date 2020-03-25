bf2d38e8c4bc5ab6bba688c01e32fc1e74263ab8
Sammuel Reis <sammugr@hotmail.com>
2020-03-24 23:14:38 -0300

    implements nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+	host: 'smtp.mailtrap.io',
+	port: 2525,
+	secure: false,
+	auth: {
+		user: '52dd260afafa1d',
+		pass: '28a32b68dc914d',
+	},
+	default: {
+		from: 'Equipe GoBarber <noreply@gobarber.com>',
+	},
+};
